bfb921ef1342337bc21e71fb894038c3e2e30153
unknown <burkeamy30@gmail.com>
2021-04-24 18:37:37 -0400

    working on responsive nav and a visual display on deploy

---
+++ src/components/menuIcon/menuIcon.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './menuIcon.styles.css';
+
+const MenuIcon = () => {
+
+
+
+    return (
+        <div class="container" >
+            <div class="bar1"></div>
+            <div class="bar2"></div>
+            <div class="bar3"></div>
+        </div>
+    )
+};
+
+export default MenuIcon;
--- src/components/top-nav/top-nav.components.jsx
+++ src/components/top-nav/top-nav.components.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './top-nav.styles.css';
 import logo from '../../images/oneMum.png'
 import { Link } from 'react-router-dom';
+import MenuIcon from '../../components/menuIcon/menuIcon.component';
 
 const TopNav = () => {
 
@@ -9,7 +10,7 @@ const TopNav = () => {
         <div className = 'top-nav'>
             <Link to='/'><img className ='logoImg' alt='flower logo' src={logo} /></Link>
             <h1>Perennial Solutions Online</h1>
-
+                <MenuIcon />
                 <nav className='options'>
                         <Link className='option' to='/'>home |</Link>
                         <Link className='option' to='/portfolio'> portfolio |</Link>

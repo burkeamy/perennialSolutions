09dfd7674417a01c30462542af20e895b3eadce6
unknown <burkeamy30@gmail.com>
2021-04-24 11:28:54 -0400

    added homepage to package.json

--- src/components/top-nav/top-nav.components.jsx
+++ src/components/top-nav/top-nav.components.jsx
@@ -3,17 +3,21 @@ import './top-nav.styles.css';
 import logo from '../../images/oneMum.png'
 import { Link } from 'react-router-dom';
 
-const TopNav = () => (
-    <div className = 'top-nav'>
-        <Link to='/'><img className ='logoImg' alt='flower logo' src={logo} /></Link>
-        <h1>Perennial Solutions Online</h1>
-        <nav className='options'>
-                <Link className='option' to='/'>home |</Link>
-                <Link className='option' to='/portfolio'> portfolio |</Link>
-                <Link className='option' to='/aboutus'> about us |</Link>
-                <Link className='option' to='/contact'> contact</Link>
-        </nav>
-    </div>
-);
+const TopNav = () => {
+
+    return (
+        <div className = 'top-nav'>
+            <Link to='/'><img className ='logoImg' alt='flower logo' src={logo} /></Link>
+            <h1>Perennial Solutions Online</h1>
+
+                <nav className='options'>
+                        <Link className='option' to='/'>home |</Link>
+                        <Link className='option' to='/portfolio'> portfolio |</Link>
+                        <Link className='option' to='/aboutus'> about us |</Link>
+                        <Link className='option' to='/contact'> contact</Link>
+                </nav>
+        </div>
+    )
+};
 
 export default TopNav;

8eaa8f40308cb3626a0687436e802a701428126e
unknown <burkeamy30@gmail.com>
2021-06-29 21:49:56 -0400

    added something to the portfolio page

--- src/pages/portfolio/portfolio.component.jsx
+++ src/pages/portfolio/portfolio.component.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-
+import AlyssasGarden from '../../images/alyssasGardenWebsite.png'
 import './portfolio.styles.css';
 
 const Portfolio = () => {
     return (
         <div>
-            <h3>future home of the portfolio</h3>
+            <h3>What are we working on now?</h3>
+            <section className='portfolio-section'>
+                <img className='screen-shot'src= {AlyssasGarden } alt ='screenshot of current project' />
+                <p>Our current project is for a small landscaping company that is just starting. 
+                    This webiste is not complete yet, but feel free to check it out. 
+                </p>
+                <a href='https://alyssasgardennc.com/' rel="noreferrer" target='_blank'>Check it out!</a>
+            </section>
         </div>
     )
 };
